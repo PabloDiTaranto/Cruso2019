a379105a2a59eadc2ad2c3c07cb4924c77c7a5b6
Pablo Di Taranto <73130438+PabloDiTaranto@users.noreply.github.com>
2022-01-07 20:24:24 -0300

    d

---
+++ FirstProject/Assets/_Scripts/MyFirstScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{
+    public int x = 4;
+
+    public int y = 3;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        AddTwoNumbers();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Adds the two numbers.
+    /// </summary>
+    private void AddTwoNumbers()
+    {
+        Debug.Log(x+y);
+    }
+}
