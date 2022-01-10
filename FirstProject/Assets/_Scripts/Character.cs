3f780ca54a029c77c0d647283440d9b699c17916
Pablo Di Taranto <73130438+PabloDiTaranto@users.noreply.github.com>
2022-01-09 23:26:12 -0300

    d

---
+++ FirstProject/Assets/_Scripts/Character.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Weapon
+{
+    public string name;
+    public int damage;
+
+    public Weapon(string name, int damage)
+    {
+        this.name = name;
+        this.damage = damage;
+    }
+
+    public void PrintWeaponStats()
+    {
+        Debug.LogFormat("Name: {0} - Damage {1}", name, damage);
+    }
+}
+
+public class Character
+{
+    public string name;
+    public int exp = 0;
+
+
+    public Character()
+    {
+        name = "Antonio Banderas";
+    }
+    
+    public Character(string name)
+    {
+        this.name = name;
+    }
+    
+    public Character(string name, int exp)
+    {
+        this.name = name;
+        this.exp = exp;
+    }
+
+    public virtual void PrintCharacterStats()
+    {
+        Debug.LogFormat("Hero: {0} - {1} EXP", name, exp);
+    }
+
+    private void Reset()
+    {
+        name = "Antonio Banderas";
+        exp = 0;
+    }
+
+}
+
+public class Paladin : Character
+{
+    public Weapon weapon;
+    
+    public Paladin() : base()
+    {
+        
+    }
+
+    public Paladin(string name, Weapon weapon) : base(name)
+    {
+        this.weapon = weapon;
+    }
+
+    public override void PrintCharacterStats()
+    {
+        Debug.LogFormat("Hola, soy el paladin {0} y llevo un(a) {1}",
+                        name, weapon.name);
+        
+    }
+}
+
+public class Archer : Character
+{
+    public Weapon arch;
+    public Archer(string name, Weapon arch) : base(name)
+    {
+        this.arch = arch;
+    }
+
+    public override void PrintCharacterStats()
+    {
+        base.PrintCharacterStats();
+        Debug.LogFormat("Llevo un(a) {0}", arch.name);
+    }
+}
+
+public class Magician : Character
+{
+    private Weapon staff;
+    public Magician(string name) : base(name)
+    {
+        staff = new Weapon("Vara de muerte", 150);
+    }
+}
--- FirstProject/Assets/_Scripts/MyFirstScript.cs
+++ FirstProject/Assets/_Scripts/MyFirstScript.cs
@@ -1,30 +1,57 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MyFirstScript : MonoBehaviour
 {
-    public int x = 4;
-
-    public int y = 3;
-    
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        AddTwoNumbers();
-    }
+        Character hero = new Character();
+        hero.PrintCharacterStats();
 
-    // Update is called once per frame
-    void Update()
-    {
+        Character hero2 = hero;
+        hero2.PrintCharacterStats();
+        hero2.name = "Madonna";
+        hero.PrintCharacterStats();
+        hero2.PrintCharacterStats();
+
+        Character heroine = new Character("Lara Croft", 10);
+        heroine.PrintCharacterStats();
+
+        Weapon sword = new Weapon("Super Sword", 10);
+        sword.PrintWeaponStats();
         
-    }
+        Weapon sword2 = sword;
+        sword2.PrintWeaponStats();
 
-    /// <summary>
-    /// Adds the two numbers.
-    /// </summary>
-    private void AddTwoNumbers()
-    {
-        Debug.Log(x+y);
+        sword2.name = "Excalibur";
+        sword2.damage = 255;
+        
+        sword.PrintWeaponStats();
+        sword2.PrintWeaponStats();
+
+        Paladin p = new Paladin();
+        p.PrintCharacterStats();
+
+        Paladin p2 = new Paladin("Arthas", sword);
+
+        Archer archer = new Archer("Legolas", new Weapon("Arco del infierno", 15));
+
+        Magician magician = new Magician("Gandalf");
+
+        List<Character> party = new List<Character>();
+        party.Add(p2);
+        party.Add(archer);
+        party.Add(magician);
+
+        foreach (Character c in party)
+        {
+            c.PrintCharacterStats();
+        }
+
+        Transform theTransform = GetComponent<Transform>();
+        Debug.Log(theTransform.position);
+        Debug.Log(theTransform.rotation);
     }
 }
