21b5203aebcb16d8b0ec7eb51f24157bb6a8cfb3
PabloDiTaranto <73130438+PabloDiTaranto@users.noreply.github.com>
2022-02-03 23:01:03 -0300

    fdn

--- Legend of Selda/Assets/_Scripts/CameraFollow.cs
+++ Legend of Selda/Assets/_Scripts/CameraFollow.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class CameraFollow : MonoBehaviour
 {
     public GameObject target;
@@ -10,6 +11,10 @@ public class CameraFollow : MonoBehaviour
     private Vector3 targetPosition;
 
     public float cameraSpeed;
+
+    private Camera theCamera;
+    private Vector3 minLimits, maxLimits;
+    public float halfHeight, halfWidth;
     
     // Start is called before the first frame update
     void Start()
@@ -17,11 +22,28 @@ public class CameraFollow : MonoBehaviour
         
     }
 
+    public void ChangeLimits(BoxCollider2D cameraLimits)
+    {
+        minLimits = cameraLimits.bounds.min;
+        maxLimits = cameraLimits.bounds.max;
+        theCamera = GetComponent<Camera>();
+        halfHeight = theCamera.orthographicSize;
+        halfWidth = halfHeight / Screen.height * Screen.width;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        targetPosition = new Vector3(target.transform.position.x,
-                                    target.transform.position.y,
+        float posX = Mathf.Clamp(target.transform.position.x,
+            minLimits.x + halfWidth,
+            maxLimits.x - halfWidth);
+
+        float posY = Mathf.Clamp(target.transform.position.y,
+            minLimits.y + halfHeight,
+            maxLimits.y - halfHeight);
+        
+        targetPosition = new Vector3(posX,
+                                    posY,
                                     transform.position.z);
     }
 
